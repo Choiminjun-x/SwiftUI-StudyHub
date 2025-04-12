d8422dc41cf05cfe5c0f30373e24681d0ce9c52c
Choiminjun <mj.choi7180@gmail.com>
2025-04-12 23:01:55 +0900

    Initial Commit

---
+++ SwiftUI-StudyHub/SwiftUI_StudyHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyHubApp.swift
+//  SwiftUI-StudyHub
+//
+//  Created by 최민준 on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
